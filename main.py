6808f53e7c38c71be3404cd304759e2b0e8139d9
vladosantic <94625684+vladosantic@users.noreply.github.com>
2022-10-11 13:10:41 +0200

    Changes to main.py

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+kaskade_lica = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+kaskade_oci = cv2.CascadeClassifier("haarcascade_eye.xml")
+kaskade_osmijeh = cv2.CascadeClassifier("haarcascade_smile.xml")
 
-WebCamFootage = cv2.VideoCapture(0)
+WebKamera = cv2.VideoCapture(0)
 
 while True:
-    ret, img = WebCamFootage.read()
+    ret, img = WebKamera.read()
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_img, 1.25, 4)
+    lica = kaskade_lica.detectMultiScale(gray_img, 1.25, 4)
 
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in lica:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-        rec_gray = gray_img[y:y + h, x:x + w]
-        rec_color = img[y:y + h, x:x + w]
+        cv2.putText(img, 'Lice', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+        snimka_siva_boja = gray_img[y:y + h, x:x + w]
+        snimka_u_boji = img[y:y + h, x:x + w]
 
-    cv2.imshow('Face Recognition', img)
+    oci = kaskade_oci.detectMultiScale(snimka_siva_boja)
+    for (ex, ey, ew, eh) in oci:
+        cv2.rectangle(snimka_u_boji, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 1)
 
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
+    cv2.imshow('Detekcija lica', img)
+
+    if cv2.waitKey(30) & 0xff == ord('q'):
         break
 
-WebCamFootage.release()
+WebKamera.release()
 cv2.destroyAllWindows()
